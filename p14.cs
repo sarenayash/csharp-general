9f409c2fdae862da5fb8d16e3f0ee9a445187303
sarenayash <sarenayash@gmail.com>
2026-02-10 10:13:02 +0530

    Optimize duplicate element detection in 2D array
    Refactored duplicate element counting logic to use a visited array for tracking counted elements.

--- p14.cs
+++ p14.cs
@@ -1,4 +1,5 @@
 using System;
+
 class Program14
 {
     public static void Main(string[] args)
@@ -8,73 +9,54 @@ class Program14
         r = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter number of columns: ");
         c = Convert.ToInt32(Console.ReadLine());
+
         int[,] arr = new int[r, c];
+        bool[,] visited = new bool[r, c];
+
         Console.WriteLine("\nEnter elements of the 2D array:");
-        for (int i=0; i<r; i++)
+        for (int i = 0; i < r; i++)
         {
-            for (int j=0; j<c; j++)
+            for (int j = 0; j < c; j++)
             {
-                Console.Write("Element [{0},{1}]: ",i,j);
-                arr[i,j]=Convert.ToInt32(Console.ReadLine());
+                Console.Write("Element [{0},{1}]: ", i, j);
+                arr[i, j] = Convert.ToInt32(Console.ReadLine());
             }
         }
-        Console.WriteLine("\n2D Array in Tabular Format:");
-        for (int i=0; i<r; i++)
+
+        Console.WriteLine("\n2D Array:");
+        for (int i = 0; i < r; i++)
         {
-            for (int j=0; j<c; j++)
+            for (int j = 0; j < c; j++)
             {
                 Console.Write(arr[i, j] + "\t");
             }
             Console.WriteLine();
         }
-        Console.WriteLine("\n\n\nDuplicate Elements and Count:");
-        bool f=false;
+        Console.WriteLine("\nDuplicate elements and their count:");
         for (int i=0; i<r; i++)
         {
             for (int j=0; j<c; j++)
             {
-                int count=1;
-                bool ac= false;
-                for (int x=0; x<=i; x++)
-                {
-                    for (int y=0; y<c; y++)
-                    {
-                        if (x==i && y==j)
-                            break;
-                        if (arr[i,j]== arr[x, y])
-                        {
-                            ac= true;
-                            break;
-                        }
-                    }
-                    if (ac)
-                        break;
-                }
-
-                if (ac)
+                if(visited[i,j])
                     continue;
+                int count=1;
+                visited[i,j]=true;
                 for (int x=i; x<r; x++)
                 {
                     for (int y=0; y<c; y++)
                     {
-                        if (x==i && y<=j)
+                        if(x==i && y<=j)
                             continue;
-
-                        if (arr[i,j]==arr[x,y])
+                        if(arr[i,j]==arr[x,y])
+                        {
                             count++;
+                            visited[x,y]=true;
+                        }
                     }
                 }
-                if (count>1)
-                {
-                    Console.WriteLine("Element {arr[0],[1]} occurs {count} times",i,j);
-                    f= true;
-                }
+                if(count>1)
+                    Console.WriteLine("Element {0} in program {1} times", arr[i, j], count);
             }
-        }
-
-        if (!f)
-        {
-            Console.WriteLine("No duplicate elements found.");
         }
     }
 }
